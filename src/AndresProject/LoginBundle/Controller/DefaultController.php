0dd426141e9b1f697282f7cea0c1a14d3f03e187
Victoria Lasso <maria.lasso@imexhs.com>
2016-05-17 00:37:35 -0500

    Modificación de redireccionamiento.

--- src/AndresProject/LoginBundle/Controller/DefaultController.php
+++ src/AndresProject/LoginBundle/Controller/DefaultController.php
@@ -8,6 +8,7 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('LoginBundle:Default:index.html.twig');
+        //return $this->render('LoginBundle:Default:index.html.twig');
+        return $this->redirect($this->generateUrl('sonata_user_admin_security_login'));
     }
 }
